ea7e850955b6b4de641e200826cde48a60711721
KOBOKOMASERO <kobokophilip@gmail.com>
2025-06-13 23:19:24 +0300

    0_float.f90

---
+++ syntax_and_arithmetic/floating_point_numbers/0_float.f90
@@ -0,0 +1,10 @@
+program float_example
+        implicit none
+        real :: a, b, c
+
+        a = 3.0
+        b = 45.0
+        c = b / a
+
+        print *, "c = ", c
+end program float_example
