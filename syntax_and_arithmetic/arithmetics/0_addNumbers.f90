a9c33dff08f48f9f11fbc7bb4e263c12b20f2cc4
KOBOKOMASERO <kobokophilip@gmail.com>
2025-06-15 03:17:12 +0300

    0_addNumbers.f90

---
+++ syntax_and_arithmetic/arithmetics/0_addNumbers.f90
@@ -0,0 +1,12 @@
+program addNumbers
+    ! adds two numbers
+    implicit none
+
+    real :: a, b, result
+
+    a = 3.0
+    b = 5.0
+
+    result = a + b
+    print *, "Result =", result
+end program addNumbers
